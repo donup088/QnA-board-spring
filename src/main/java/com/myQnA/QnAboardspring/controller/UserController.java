b63696ee6e967be9661b84c383b03990f713f7e3
Donghee <donup08@naver.com>
2020-07-17 00:13:24 +0900

    회원가입, 사용자 목록 기능 구현

---
+++ src/main/java/com/myQnA/QnAboardspring/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.myQnA.QnAboardspring.controller;
+
+import com.myQnA.QnAboardspring.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UserController {
+    private List<User> users=new ArrayList<>();
+
+    @PostMapping("/create")
+    public String create(User user){
+        System.out.println("User : "+user);
+        users.add(user);
+        return "redirect:/list";
+    }
+    @GetMapping("/list")
+    public String list(Model model){
+        model.addAttribute("users",users);
+        return "list";
+    }
+}
