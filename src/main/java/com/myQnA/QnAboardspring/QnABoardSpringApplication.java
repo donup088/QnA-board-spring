9abbde7a1427ee011ec743eb1b797fbb478c9b34
Donghee <donup08@naver.com>
2020-07-16 17:17:40 +0900

    init

---
+++ src/main/java/com/myQnA/QnAboardspring/QnABoardSpringApplication.java
@@ -0,0 +1,13 @@
+package com.myQnA.QnAboardspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnABoardSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QnABoardSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myQnA/QnAboardspring/QnABoardSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myQnA.QnAboardspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnABoardSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
